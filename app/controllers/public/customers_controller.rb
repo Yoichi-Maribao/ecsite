093fdb6e5727f01d12c7d09875ce4a54a5401536
Yoichi-Maribao <szk.m.yo1@gmail.com>
2021-04-18 11:00:45 +0100

    [complete]アカウント管理

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,5 @@
 class Public::CustomersController < ApplicationController
-  before_action :authenticate_end_user!
+  before_action :authenticate_end_user!, :check_login
   def show
     @user = current_end_user
   end
@@ -8,11 +8,26 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
+    @user = current_end_user
+    @user.update(end_user_params)
+    redirect_to customers_mypage_path
   end
 
   def unsubscribe
   end
 
   def withdraw
+    @user = current_end_user
+    @user.update(is_active: false)
+    redirect_to logout_path
+  end
+
+  private
+  def end_user_params
+    params.require(:end_user).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :email, :postcode, :address, :phone_number)
+  end
+
+  def check_login
+    redirect_to logout_path if current_end_user.is_active != true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :customers, except: [:create, :new, :destroy]
+  end
   devise_for :admins, path: :admin, skip: 'registrations'
-  devise_for :end_users, parh: :public
-
+  devise_for :end_users
+  devise_scope :end_user do
+    get '/logout', to: 'devise/sessions#destroy', as: :logout
+  end
   namespace :admin do
     get '/', to: 'homes#top'
   end
   scope module: :public do
     get 'customers/mypage', to: 'customers#show'
     get 'customers/edit'
-    patch 'customers/update'
-    delete 'customers/unsubscribe'
-    delete 'customers/withdraw'
+    patch 'customers', to: 'customers#update'
+    get 'customers/unsubscribe'
+    patch 'customers/withdraw', to: 'customers#withdraw'
   end
 
   root to: 'public/homes#top'

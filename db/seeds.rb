ad5334b9593b5ce63e6d13fcf4cfdcf5b0539c56
Yoichi-Maribao <szk.m.yo1@gmail.com>
2021-04-19 07:20:19 +0100

    [Update] added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,43 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Admin.create!(
-#   :email => "admin@email.com",
-#   :password => "aaaaaa"
-# )
+Admin.create!(
+  :email => "admin@email.com",
+  :password => "aaaaaa"
+)
+
+Genre.create!(
+  :name => "ケーキ"
+)
+
+Genre.create!(
+  :name => "クッキー"
+)
 
 Item.create!(
   :genre_id => 1,
   :name => "シフォンケーキ",
   :description => "みんな大好きシフォンケーキです。ほっぺたがおちるくらい美味しいです。",
-  :image_id => nil,
+  :image => open("./app/assets/images/cake.jpeg"),
   :price => 600
 )
-# End_User.create!(
-#   last_name: "試験"
-#   first_name: "太郎"
-#   kana_last_name: "シケン"
-#   kana_first_name: "タロウ"
-#   email: "a@a"
-#   postcode: "0000000"
-#   address: "東京都豊島区南大塚"
-#   phone_number: "00000000000"
-#   encrypted_password: "testing"
-# )
+
+Item.create!(
+  :genre_id => 2,
+  :name => "お手軽クッキー",
+  :description => "お手軽に食べれるクッキーです。",
+  :image => open("./app/assets/images/cookie.jpeg"),
+  :price => 300
+)
+
+EndUser.create!(
+  last_name: "試験",
+  first_name: "太郎",
+  kana_last_name: "シケン",
+  kana_first_name: "タロウ",
+  email: "aa@a",
+  postcode: "0000000",
+  address: "東京都豊島区",
+  phone_number: "00000000000",
+  password: "testing",
+)
